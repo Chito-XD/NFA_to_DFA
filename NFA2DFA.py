01d7a3ad3f932aad25bc49d7c001067f846cd5ff
Peredo <peredo98@hotmail.com>
2019-10-24 22:00:25 -0500

    bug fix

--- NFA2DFA.py
+++ NFA2DFA.py
@@ -1,3 +1,13 @@
+#  http://pythonfiddle.com/dfa-simple-implementation/
+import re
+from itertools import chain
+
+states_NFA = set()  # All the states of the NFA 
+input_datum_NFA = set() # All the possible inputs for the NFA
+nfa_trans_fun = dict()
+dfa_trans_fun = dict()
+conjunto_potencia = []
+
 def isContained(cp, newVal):
     n = len(cp)
     k = len(newVal)
@@ -12,22 +22,16 @@ def isContained(cp, newVal):
 
     return False
     
+def nfa2dfa(nfakeys): # Method to make the set of subsets for the DFA
+    
+    global conjunto_potencia
 
-def nfa2dfa(nfa):
-    n =  len(nfa.keys())
-    print(n)
-    k = 2**n -1
+    n = len(nfakeys)
 
-    alphabet = []
-    nfakeys = list(nfa)
-    print(nfakeys)
     for i in range(n):
         val = nfakeys[i]
-        alphabet.append([val])
+        conjunto_potencia.append([val])
 
-    alphabet = sorted(alphabet)
-    print(alphabet)
-    conjunto_potencia = alphabet
     i = 0
     while i < len(conjunto_potencia):
         j = 0 
@@ -39,9 +43,120 @@ def nfa2dfa(nfa):
                 i = 0
             j += 1
         i += 1
-    print(conjunto_potencia)
+    
+
+def getValue(datum, element):
+    global nfa_trans_fun
+    global dfa_trans_fun
+
+    try:
+        # The value is not null in the NFA
+        temp = nfa_trans_fun[datum, element]
+        value = set(temp)
+    except:
+        # the value is null in NFA
+        value = None
+    
+    return value
 
 
-nfa = {"q0" : {"0" : {"q0", "q1"}, "1": {"q0"} }, "q1" : { "0" : {}, "1" : {"q2"}}, "q2" : {"0" : {}, "1": {}}}
-print(nfa)
-nfa2dfa(nfa)
+def make_dfa_trans_fun():
+    global conjunto_potencia
+    global input_datum_NFA
+    global nfa_trans_fun
+    global dfa_trans_fun
+    
+    # First, the empty string
+    for datum in input_datum_NFA:
+        dfa_trans_fun[datum, ''] = { }
+
+
+    for subset in conjunto_potencia:
+        print('the subset is')
+        print(subset)
+        for datum in input_datum_NFA:
+            print('the datum is:')
+            print(datum)
+            values = set()
+            for element in subset:
+                print('the element is:')
+                print(element)
+                tempValues = getValue(datum, element)
+                if tempValues != None:
+                    values = set(chain(values, tempValues))
+            print('the values are')
+            print(values)
+            if len(values) == 0:
+                dfa_trans_fun[datum, key1] = { }
+            else:
+                key1 = ""
+                key1 = ', '.join([str(el) for el in subset])
+                dfa_trans_fun[datum, key1] = values
+    
+
+def make_nfa_trans_fun(reg):
+    
+    allTrans = re.findall("[^\(|\)|\,]+", reg);
+     # allTrans[i*3]  -> The input for the NFA
+    # allTrans[i*3+1]  -> The state you start
+    # allTrans[i*3+2]  -> The state you move to
+
+    global states_NFA
+    global input_datum_NFA
+    
+    for i in range(len(allTrans)//3):
+        try: #Check if the input and state already exits
+            temp = nfa_trans_fun[allTrans[i*3],allTrans[i*3+1]]
+            temp.append(allTrans[i*3+2])
+            nfa_trans_fun[allTrans[i*3],allTrans[i*3+1]] = temp
+        except:
+            nfa_trans_fun[allTrans[i*3],allTrans[i*3+1]] = [allTrans[i*3+2]]
+
+        states_NFA.add(allTrans[i*3+1])
+        input_datum_NFA.add(allTrans[i*3])
+    
+    states_NFA = sorted(list(states_NFA))
+    input_datum_NFA = sorted(list(input_datum_NFA))
+
+    
+def print_trans_fun(trans_func):
+    print("The transition function is: ")
+    for keys,values in trans_func.items():
+        transition = "   {} -> {}".format(keys, list(values))
+        print(transition)
+    print()
+
+def createFile():
+    global dfa_trans_fun
+    path = "./files/writeFile.txt"
+    f = open(path, "w+")
+    for keys,values in dfa_trans_fun.items():
+        transition = "{} -> {}".format(keys, list(values))
+        f.write("%s\n" % (transition))
+    f.close()
+
+
+def readFile():
+    path = "./files/test_file.txt"
+    f = open(path, "r")
+    if f.mode == "r":
+        contents = f.read()
+    x = re.findall("(\(.+\))", contents)[0];
+    return x
+    
+contentFile = readFile()
+make_nfa_trans_fun(contentFile)
+print_trans_fun(nfa_trans_fun)
+print(states_NFA)
+print(input_datum_NFA)
+nfa2dfa(states_NFA)
+print(conjunto_potencia)
+print("el conjunto potencia es de longitud {}".format(len(conjunto_potencia)))
+
+print('----')
+print('bien lo bueno')
+
+make_dfa_trans_fun()
+print('la funcion del dfa esssssssssssssssssssssssssssssssssssssss:')
+print_trans_fun(dfa_trans_fun)
+createFile()
