25fc0ae6dbb738641b485f4732f88939c5444e4e
Chito-XD <chito99salu@outlook.com>
2019-11-02 20:25:21 -0600

    done

--- Main.py
+++ Main.py
@@ -1,12 +1,14 @@
-#  http://pythonfiddle.com/dfa-simple-implementation/
 import re
 from itertools import chain
 
 states_NFA = set()  # All the states of the NFA 
+states_DFA = set() # All the states of the DFA
 input_datum_NFA = set() # All the possible inputs for the NFA
 nfa_trans_fun = dict()
 dfa_trans_fun = dict()
 conjunto_potencia = []
+accept_states = []
+q0 = []
 
 def isContained(cp, newVal):
     n = len(cp)
@@ -44,6 +46,10 @@ def nfa2dfa(nfakeys): # Method to make the set of subsets for the DFA
             j += 1
         i += 1
     
+    conjunto_potencia = sorted(conjunto_potencia, key = sortConjunto)
+
+def sortConjunto(x):
+    return len(x)
 
 def getValue(datum, element):
     global nfa_trans_fun
@@ -72,26 +78,19 @@ def make_dfa_trans_fun():
 
 
     for subset in conjunto_potencia:
-        print('the subset is')
-        print(subset)
         for datum in input_datum_NFA:
-            print('the datum is:')
-            print(datum)
             values = set()
             for element in subset:
-                print('the element is:')
-                print(element)
                 tempValues = getValue(datum, element)
                 if tempValues != None:
                     values = set(chain(values, tempValues))
-            print('the values are')
-            print(values)
             if len(values) == 0:
                 dfa_trans_fun[datum, key1] = { }
             else:
                 key1 = ""
                 key1 = ', '.join([str(el) for el in subset])
                 dfa_trans_fun[datum, key1] = values
+            states_DFA.add(key1)
     
 
 def make_nfa_trans_fun(reg):
@@ -120,18 +119,36 @@ def make_nfa_trans_fun(reg):
 
     
 def print_trans_fun(trans_func):
-    print("The transition function is: ")
     for keys,values in trans_func.items():
-        transition = "   {} -> {}".format(keys, values)
+        transition = "   {} -> {}".format(keys, list(values))
         print(transition)
     print()
 
 def createFile():
     global dfa_trans_fun
+    global accept_states
+
     path = "./files/writeFile.txt"
     f = open(path, "w+")
+    states = "{}".format((conjunto_potencia))
+    f.write("The states for the DFA are:\n");
+    f.write("%s\n\n" %(states));
+
+    f.write("The alphabet is: ");
+    alfa = "{}".format(input_datum_NFA);
+    f.write("%s\n\n" %(alfa));
+    
+    f.write("The accepted states are: ");
+    ac_st = "{}".format(accept_states);
+    f.write("%s\n\n" %(ac_st));
+
+    f.write("The initial state is: ");
+    q = "{}".format(q0);
+    f.write("%s\n\n" %(q));
+
+    f.write("The transition function is: \n");
     for keys,values in dfa_trans_fun.items():
-        transition = "{} -> {}".format(keys, values)
+        transition = "{} -> {}".format(keys, list(values))
         f.write("%s\n" % (transition))
     f.close()
 
@@ -143,20 +160,35 @@ def readFile():
         contents = f.read()
     x = re.findall("(\(.+\))", contents)[0];
     return x
-    
-contentFile = readFile()
-make_nfa_trans_fun(contentFile)
-print_trans_fun(nfa_trans_fun)
-print(states_NFA)
-print(input_datum_NFA)
-nfa2dfa(states_NFA)
-print(conjunto_potencia)
-print("el conjunto potencia es de longitud {}".format(len(conjunto_potencia)))
 
-print('----')
-print('bien lo bueno')
+
+def findAcceptStates(accept):
+    global conjunto_potencia
+
+    global accept_states
+
+    for subset in conjunto_potencia:
+        if accept in subset:
+            accept_states.append(subset)
+
+
+    
+
+# MAIN METHOD #
+inicial = input("¿Cual es el estado inicial? \n")
+q0.append(inicial)
+
+accept_state = input("¿Cual es el estado de aceptacion? \n")
+
+contentFile = readFile()
+
+make_nfa_trans_fun(contentFile)
+
+nfa2dfa(states_NFA)
 
 make_dfa_trans_fun()
-print('la funcion del dfa esssssssssssssssssssssssssssssssssssssss:')
-print_trans_fun(dfa_trans_fun)
+
+findAcceptStates(accept_state)
+
 createFile()
+
--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # NFA_to_DFA
 
-This project convert a NFA to its correct DFA
+This project converts a NFA to its correct DFA
+
+## Instructions
+
+In order to run the program correctly it is neccessary a txt file which store the transition function of an NFA. This file must be inside the `files` folder.
+<br> The format the txt file must have is the following: 
+
+        {(0,p,p),(0,p,q),(1,p,p),(0,q,r),(1,q,r),(0,r,s),(0,s,s),(1,s,s)}
+
+This represent a transition function which has three elements. The input, the actual state and the final state respectively. It must be done for every single element of the NFA. 
+
+- New txt file
+
+If you want to test a different NFA from a different file it has to be as said, in the `files` folder and insert the new file name ino the path in the `readFile` method. Here is how: 
+
+    def readFile():
+        path = "./files/<<filename>>.txt"
+        f = open(path, "r")
+        if f.mode == "r":
+        contents = f.read()
+        x = re.findall("(\(.+\))", contents)[0];
+        return x
